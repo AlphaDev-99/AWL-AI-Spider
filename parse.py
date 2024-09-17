5b09dcb4aa5fa05cb5b6b60e507086ee99cabd37
AlphaDev-99 <diannam.paler@gmail.com>
2024-09-18 06:49:18 +0800

    Update parse.py

--- parse.py
+++ parse.py
@@ -1,7 +1,6 @@
 from langchain_ollama import OllamaLLM
 from langchain_core.prompts import ChatPromptTemplate
 
-
 template = (
     "You are tasked with extracting specific information from the following text content: {dom_content}. "
     "Please follow these instructions carefully: \n\n"
@@ -9,26 +8,22 @@ template = (
     "2. **No Extra Content:** Do not include any additional text, comments, or explanations in your response. "
     "3. **Empty Response:** If no information matches the description, return an empty string ('')."
     "4. **Direct Data Only:** Your output should contain only the data that is explicitly requested, with no other text."
-    "5. **Prioritize Accuracy:** Ensure that the data provided is as accurate and specific to the request as possible."
 )
 
-
-model = OllamaLLM(model="llama3.1")
+model = OllamaLLM(model="llama3")
 
 
 def parse_with_ollama(dom_chunks, parse_description):
     prompt = ChatPromptTemplate.from_template(template)
     chain = prompt | model
 
-
     parsed_results = []
 
-
     for i, chunk in enumerate(dom_chunks, start=1):
-        response = chain.invoke({"dom_content": chunk, "parse_description": parse_description}
+        response = chain.invoke(
+            {"dom_content": chunk, "parse_description": parse_description}
         )
-        print(f"Parsed batch {i} of {len(dom_chunks)}")
+        print(f"Parsed batch: {i} of {len(dom_chunks)}")
         parsed_results.append(response)
-
 
     return "\n".join(parsed_results)
